57e4a84703d08d1ebfda2938676456abee9090bb
Kavin <62742678+Kavinraja-G@users.noreply.github.com>
2024-08-01 23:25:23 +0530

    Support running conftest in action/exec

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 const core = require('@actions/core')
+const exec = require('@actions/exec')
 const fs = require('fs')
 const yaml = require('js-yaml')
 
 async function run() {
   try {
     const configStr = core.getInput('config')
+    const tfPlanOutputPath = core.getInput('tfPlanOutputPath')
 
     const config = yaml.load(configStr)
 
+    // Write config.yaml for OPA Inputs
     const opaInputContent = yaml.dump({
       config: {
         ec2: {
@@ -22,10 +25,23 @@ async function run() {
     })
 
     const outputPath = 'config.yaml'
-
     fs.writeFileSync(outputPath, opaInputContent, 'utf8')
 
     core.setOutput('out-file', outputPath)
+
+    // Run conftest
+    try {
+      await exec.exec('conftest', [
+        'test',
+        tfPlanOutputPath,
+        '-d',
+        outputPath,
+        '--policy',
+        'policy/'
+      ])
+    } catch (error) {
+      core.setFailed(`Conftest failed: ${error.message}`)
+    }
   } catch (error) {
     core.setFailed(`Action failed with error: ${error}`)
   }
